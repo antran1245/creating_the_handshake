71146a3c3b723e7b70a18399ff678ee9257a9793
antran1245 <antran1245@gmail.com>
2022-05-16 12:45:15 -0700

    move socket back into useEffect comment out return

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -5,14 +5,14 @@ import io from 'socket.io-client';
 export default () => {
     const [socket] = useState(() => io('http://localhost:8000', {cors: {origin: "*"}}));
 
-    socket.on('connect', () => {
-        console.log('connect')
-        socket.on("welcome", (data) => console.log(data));
-        // socket.emit('back', 'hello from client');
-    })
     useEffect(() => {
+        socket.on('connect', () => {
+            console.log('connect')
+            socket.on("welcome", (data) => console.log(data));
+            // socket.emit('back', 'hello from client');
+        })
         console.log('Is this running? ');
-        return () => socket.disconnect(true);
+        // return () => socket.disconnect(true);
     }, [socket]);
 
     return(
